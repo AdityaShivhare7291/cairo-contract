a0e4d38705a6d1f8cdd16b943fc3ec8cdd9d340a
adityashivhare <96012505+AdityaShivhare7291@users.noreply.github.com>
2024-09-06 17:56:11 +0530

    Step 8 (#6)
    * final updates
    
    * deployment updates

---
+++ Counter_workshop-main/scripts/utils.ts
@@ -0,0 +1,29 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+export async function getCompiledCode(filename: string) {
+  const sierraFilePath = path.join(
+    __dirname,
+    `workshop/target/dev/${filename}.compiled_contract_class.json`
+  );
+  console.log("casmFilePath=", sierraFilePath);
+
+  console.log("sierraFilePath=", sierraFilePath);
+  const casmFilePath = path.join(
+    __dirname,
+    `workshop/target/dev/${filename}.compiled_contract_class.json`
+  );
+  console.log("casmFilePath=", casmFilePath);
+
+  const code = [sierraFilePath, casmFilePath].map(async (filePath) => {
+    const file = await fs.readFile(filePath);
+    return JSON.parse(file.toString("ascii"));
+  });
+
+  const [sierraCode, casmCode] = await Promise.all(code);
+
+  return {
+    sierraCode,
+    casmCode,
+  };
+}
